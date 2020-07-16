73709a3c883d7e5b2a8147c081a39912f7385ecb
DanRud <danielrudziewicz96@gmail.com>
2020-07-16 14:39:48 +0200

    fibonacci

---
+++ 13_Fibonacci.py
@@ -0,0 +1,18 @@
+# Fibonacci 1, 1, 2, 3, 5, 8, 13, 21...
+
+num = int(input('How many Fibonacci numbers you would like to see '))
+
+def fib(num):
+    a = 0
+    b = 1
+    if num < 2:
+        return 1
+    else:
+        for i in range(0, num):
+            c = a + b
+            a = b
+            b = c
+            print(b)
+fib(num)
+        
+        
